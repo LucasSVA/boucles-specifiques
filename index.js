945bb5b2bc6ae35f70e55edd365ebed9f3365407
LucasSVA <lucas.svanadze@gmail.com>
2022-06-28 18:51:38 +0200

    exercise 8 done - thanks to Julie :)

--- index.js
+++ index.js
@@ -148,3 +148,20 @@
 // })
 
 // console.log(pie);
+
+
+// ---- ex - 7 
+
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// let result = 1
+
+//  array.forEach((element) => {
+
+//  result *= element
+   
+
+//  })
+
+// console.log(result);
