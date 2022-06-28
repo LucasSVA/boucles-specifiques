aed115a24df323d0d4f0bc4fc27a661286d4f0f9
LucasSVA <lucas.svanadze@gmail.com>
2022-06-28 14:55:56 +0200

    exercise 2

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 //  ---- ex 1
 
-let array = [1, 2, 3, 4, 5]
+// let array = [1, 2, 3, 4, 5]
 
-let double = array.map((array, index) => {
-    return array * 2
+// let double = array.map((array) => {
+//     return array * 2
+// })
+// console.log(double);
+
+
+// ---- ex 2
+
+const longNames = [
+    {
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
+    }
+]
+
+let shortName = longNames.map((element) => {
+
+
+
+    console.log(element.firstName);
+    return { name: `${element.firstName} ${element.lastName}` }
+
+
 })
-console.log(double);
+console.log(shortName);
